b78c4c3ba2eac0e6d541503211bb1d1c2053fce6
Ste Rose <git@sterose.io>
2024-09-08 21:37:12 +0100

    testing concat of strings

---
+++ baseline/string-concat-basics.py
@@ -0,0 +1,8 @@
+# string concatenation
+
+# the , operator inserts a space
+print ("Python","is","fun?")
+
+# the + operator does not, so the space must be within the string
+print ("Welcome to "+"Python")
+print ("Py" + "thon")
