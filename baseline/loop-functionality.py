9e44385821db234e91458d8c051d173563abcb35
Ste Rose <git@sterose.io>
2024-09-08 22:11:46 +0100

    loop basics and fibonacci challenge script

---
+++ baseline/loop-functionality.py
@@ -0,0 +1,35 @@
+# using for loops in different scenarios
+
+# loop through each character in a string
+number = 0
+for letter in "string_of_text":
+    number = number + 1
+    print(letter , number)
+
+
+# print 6 times in loop using range, and once outside the loop (non-indented)
+for i in range(6):
+    print("again")
+print("again")
+
+
+# prints 10, 9, 8 - python is zeio-indexed so counts from 0
+num = 10
+for i in range(3):
+    print(num - i)
+
+
+# fibonacci challenge - calculate to at least the 20th term based on any two starting values
+
+
+print("This is the beginnig of the fibonacci challenge code")
+a = 2
+print(a)
+b = 5
+print(b)
+for i in range(18):
+    c = a + b
+    print(c)
+    a = b
+    b = c
+
