b100db7d5612801f7c6398cb937c9e715693d79c
Ste Rose <git@sterose.io>
2024-10-11 18:18:24 +0100

    Write a basic conversion tool

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+print("----------------------------------------------")
+print("-------   Weight Calculator  -----------------")
+print("----------------------------------------------")
+
+weight = input("Weight: ")
+measure = input("(K)g or (L)bs: ")
+
+if measure == "K":
+    print("Converting to LBS ...")
+    sum = int(weight) * 2.2
+    print(sum)
+else:
+    print("Converting to KGS ...")
+    sum = int(int(weight) / 2.2)
+    print(sum)
+    
